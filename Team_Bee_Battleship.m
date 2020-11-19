0054694105a358f53a2486a720b6f2cba4d0954d
Deakon <deakonziegler@gmail.com>
2020-11-19 11:53:15 -0500

    test

--- Team_Bee_Battleship.m
+++ Team_Bee_Battleship.m
@@ -35,35 +35,15 @@ drawScene(battleshipScene, boardDisplay)
 % Place the left pointing end of the ship at position (2,3)
 boardDisplay(2,3) = left_ship_sprite;
 
+%Redraw the scene.
 drawScene(battleshipScene,boardDisplay)
 
+%Call the player shoot function and assign the output to the shot display.
 shotDisplay = playerShoot(battleshipScene, boardDisplay, shotDisplay);
 
+%Redraw the scene.
 drawScene(battleshipScene, boardDisplay, shotDisplay)
 
-%% Functions
+m = ones(10, 10)
 
-%Prompts the player to pick a location to shoot.
-function shotDisplay = playerShoot(obj, boardDisplay, shotDisplay)
-
-%Initial prompt for player to choose a tile.
-[shotRow, shotCol, shotButton] = getMouseInput(obj);
-
-%Loop while the mouse button isn't MOUSE 1, or if the value at the shotRow
-%and shotCol is less than 2, or if the shotCol is less than 12.
-while shotButton ~= 1 || boardDisplay(shotRow, shotCol) < 2 || shotCol < 12
-    %Prompt the player to choose another tile.
-    [shotRow, shotCol, shotButton] = getMouseInput(obj);
-    
-end
-
-%Check if the location clicked by the player is a ship sprite.
-if boardDisplay(shotRow, shotCol) > 2 && boardDisplay(shotRow, shotCol) < 9
-    %Add the hit sprite to the shotDisplay matrix.
-    shotDisplay(shotRow, shotCol) = 9;
-else
-    %Add the miss sprite to the shotDisplay matrix.
-    shotDisplay(shotRow, shotCol) = 10;
-end
-
-end
+s = checkSunk(battleshipScene, shotDisplay, m)
