3a541bc66ec9fbae4b992d5dfc6a874411dfe565
Deakon <deakonziegler@gmail.com>
2020-11-22 13:04:41 -0500

    Moved the player ship placement into it's own 'playerPlacement.m' function.

---
+++ playerPlacement.m
@@ -0,0 +1,181 @@
+function [board_display, ship_matrix] = playerPlacement(obj, board_display, ship_matrix)
+%PLAYERSHIPS Summary of this function goes here
+%   Detailed explanation goes here
+
+%Declaration of ship sprite variables with numerical values.
+left_ship_sprite = 3;
+horiz_ship_sprite = 4;
+right_ship_sprite = 5;
+top_ship_sprite = 6;
+vert_ship_sprite = 7;
+bot_ship_sprite = 8;
+
+%build the first ship
+vertical = 0;
+ship_complete = 0;
+finished_matrix = build_ship(obj,5,ship_matrix);
+%these nested loops take the function outputted matrix and uses it to change the board matrix to show the ships
+for i = 1:10
+   for j = 1:10
+      if finished_matrix(i,j) == 5
+         if finished_matrix(i+1,j) == finished_matrix(i,j)
+             vertical = 1;
+         end
+         if vertical == 1
+             board_display(i,j) = top_ship_sprite;
+             board_display(i+1,j) = vert_ship_sprite;
+             board_display(i+2,j) = vert_ship_sprite;
+             board_display(i+3,j) = vert_ship_sprite;
+             board_display(i+4,j) = bot_ship_sprite;
+             ship_complete = 1;
+         else
+             board_display(i,j) = left_ship_sprite;
+             board_display(i,j+1) = horiz_ship_sprite;
+             board_display(i,j+2) = horiz_ship_sprite;
+             board_display(i,j+3) = horiz_ship_sprite;
+             board_display(i,j+4) = right_ship_sprite;
+             ship_complete = 1;
+         end
+      end
+      if(ship_complete == 1)
+          break
+      end
+   end
+   if (ship_complete ==1)
+       break
+   end
+end
+drawScene(obj,board_display)
+
+%build ship 2
+finished_matrix = build_ship(obj,4,finished_matrix);
+vertical = 0;
+ship_complete = 0;
+for i = 1:10
+   for j = 1:10
+      if finished_matrix(i,j) == 4
+         if finished_matrix(i+1,j) == finished_matrix(i,j)
+             vertical = 1;
+         end
+         if vertical == 1
+             board_display(i,j) = top_ship_sprite;
+             board_display(i+1,j) = vert_ship_sprite;
+             board_display(i+2,j) = vert_ship_sprite;
+             board_display(i+3,j) = bot_ship_sprite;
+             ship_complete = 1;
+         else
+             board_display(i,j) = left_ship_sprite;
+             board_display(i,j+1) = horiz_ship_sprite;
+             board_display(i,j+2) = horiz_ship_sprite;
+             board_display(i,j+3) = right_ship_sprite;
+             ship_complete = 1;
+         end
+      end
+      if(ship_complete == 1)
+          break
+      end
+   end
+   if (ship_complete ==1)
+       break
+   end
+end
+drawScene(obj,board_display)
+
+%build ship 3
+vertical = 0;
+ship_complete = 0;
+finished_matrix = build_ship(obj,3,ship_matrix);
+for i = 1:10
+   for j = 1:10
+      if finished_matrix(i,j) == 3
+         if finished_matrix(i+1,j) == finished_matrix(i,j)
+             vertical = 1;
+         end
+         if vertical == 1
+             board_display(i,j) = top_ship_sprite;
+             board_display(i+1,j) = vert_ship_sprite;
+             board_display(i+2,j) = bot_ship_sprite;
+             ship_complete = 1;
+         else
+             board_display(i,j) = left_ship_sprite;
+             board_display(i,j+1) = horiz_ship_sprite;
+             board_display(i,j+2) = right_ship_sprite;
+             ship_complete = 1;
+         end
+      end
+      if(ship_complete == 1)
+          break
+      end
+   end
+   if (ship_complete ==1)
+       break
+   end
+end
+drawScene(obj,board_display)
+
+%build ship 4
+vertical = 0;
+ship_complete = 0;
+finished_matrix = build_ship(obj,3,ship_matrix);
+for i = 1:10
+   for j = 1:10
+      if finished_matrix(i,j) == 3
+         if finished_matrix(i+1,j) == finished_matrix(i,j)
+             vertical = 1;
+         end
+         if vertical == 1
+             board_display(i,j) = top_ship_sprite;
+             board_display(i+1,j) = vert_ship_sprite;
+             board_display(i+2,j) = bot_ship_sprite;
+             ship_complete = 1;
+         else
+             board_display(i,j) = left_ship_sprite;
+             board_display(i,j+1) = horiz_ship_sprite;
+             board_display(i,j+2) = right_ship_sprite;
+             ship_complete = 1;
+         end
+      end
+      if(ship_complete == 1)
+          break
+      end
+   end
+   if (ship_complete ==1)
+       break
+   end
+end
+drawScene(obj,board_display)
+
+
+
+vertical = 0;
+ship_complete = 0;
+finished_matrix = build_ship(obj,2,ship_matrix);
+for i = 1:10
+   for j = 1:10
+      if finished_matrix(i,j) == 2
+         if finished_matrix(i+1,j) == finished_matrix(i,j)
+             vertical = 1;
+         end
+         if vertical == 1
+             board_display(i,j) = top_ship_sprite;
+             board_display(i+1,j) = bot_ship_sprite;
+             ship_complete = 1;
+         else
+             board_display(i,j) = left_ship_sprite;
+             board_display(i,j+1) = right_ship_sprite;
+             ship_complete = 1;
+         end
+      end
+      if(ship_complete == 1)
+          break
+      end
+   end
+   if (ship_complete ==1)
+       break
+   end
+end
+
+drawScene(obj,board_display)
+
+end %End Function
+
